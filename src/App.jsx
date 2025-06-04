6d2d77f9629d60e5b65b67c665bdfcdf297dd9cc
Saniya Ahmad <saniya19019@gmail.com>
2025-06-04 08:29:41 +0530

    This is first commit

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+
+const App = () => {
+  const [city, setCity] = useState('Vadodara');
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState('');
+
+  const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+
+  const fetchWeather = async () => {
+    try {
+      const res = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+      );
+      const data = res.data;
+      if (data.cod === 200) {
+        setWeather(data);
+        setError('');
+      } else {
+        setError('City not found 😢');
+        setWeather(null);
+      }
+    } catch (err) {
+      setError('Something went wrong 😵');
+    }
+  };
+
+  useEffect(() => {
+    fetchWeather();
+  }, []);
+
+  return (
+    <main>
+      <div className='h-screen w-screen flex justify-center items-center bg-[url("https://images.unsplash.com/photo-1696355360215-b7bacb6ed736?q=80&w=2176&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")] bg-cover bg-center'>
+        <div className="h-auto w-[350px] flex flex-col justify-start items-center p-6 bg-transparent shadow-black backdrop-blur-xs bg-opacity-80 rounded-lg shadow-lg text-center">
+          <h2 className="text-2xl font-bold text-blue-700 mb-4">🌤️ Weather Forecast</h2>
+
+          <input
+            type="text"
+            placeholder="Enter city name..."
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            className="p-2 w-full rounded-md border border-gray-300 mb-3"
+          />
+          <button
+            onClick={fetchWeather}
+            className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded mb-4"
+          >
+            Get Weather
+          </button>
+
+          {error && <p className="text-red-600 font-semibold">{error}</p>}
+
+          {weather && (
+            <div className="text-gray-800">
+              <h3 className="text-xl font-semibold mb-1">{weather.name} 🌍</h3>
+              <p>🌡️ Temp: {weather.main.temp}°C</p>
+              <p>🌥️ Condition: {weather.weather[0].main}</p>
+              <p>💨 Wind: {weather.wind.speed} m/s</p>
+              <p>💧 Humidity: {weather.main.humidity}%</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default App;
